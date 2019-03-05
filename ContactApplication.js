dc97db423171105e568b57c9f4e0cd362d968a65
YoyaMohamed <aya.yoya962015@gmail.com>
2019-03-05 09:17:26 +0200

    Edit Funciton

--- ContactApplication.js
+++ ContactApplication.js
@@ -1,15 +1,25 @@
+
+var contactHeader;
 function getContactDetails(obj){
 	var result=$(obj).children();
-	var contactHeader=result[1].innerHTML;
+	contactHeader=result[1].innerHTML;
 	document.getElementById("contactHeader").innerHTML=contactHeader;
 	
 	var contactImg=result[0].getAttribute('src');
 
 	
 	document.getElementById("contactImg").setAttribute('src',contactImg);
-	// alert(result[2]);
+	alert(result[2]);
 	//var phone=result[2].getAttribute('href');
    
 	//document.getElementById("callButton").setAttribute('href',phone);
+
+	
+}
+
+function EditContactDetails(){
+	//alert(contactHeader);
+	document.getElementById("name").value=contactHeader;
+	
 	
 }
